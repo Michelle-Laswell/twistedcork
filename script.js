171493f9f1a6fb5d83d4454cb05566cfc5d8c3c0
Michelle Laswell <michelle.laswell@gmail.com>
2024-06-08 20:39:35 -0400

    revisions of existing files and addition of new files

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function openMenu(evt, menuName) {
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    
+
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < x.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
     }
-    
+
     document.getElementById(menuName).style.display = "block";
     evt.currentTarget.firstElementChild.className += " w3-dark-grey";
 }
