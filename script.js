2433c6a4101ecd326c378c6f7a232b4588df168f
Michelle Laswell <michelle.laswell@gmail.com>
2024-06-29 19:42:02 -0400

    Finished content of the menu page and started content of the events page

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-// Tabbed Menu
+//Putting the Current Date in the Placeholder
+        
+window.onload = function() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = ('0' + (now.getMonth() + 1)).slice(-2);
+    var day = ('0' + now.getDate()).slice(-2);
+    var hours = ('0' + now.getHours()).slice(-2);
+    var minutes = ('0' + now.getMinutes()).slice(-2);
+
+    var formattedDate = `${year}-${month}-${day}T${hours}:${minutes}`;
+
+    document.getElementById('reservation-date').value = formattedDate;
+};
+
+//Shows active page in the navbar
+
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('.navbar a');
+    links.forEach(function(link) {
+        if (link.href === window.location.href) {
+            link.classList.add('active');
+        }
+    });
+});
+
+
+// Tabbed Menu on the menu page
 function openMenu(evt, menuName) {
     var i, x, tablinks;
     x = document.getElementsByClassName("menu");
