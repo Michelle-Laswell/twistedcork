19a085bd238396a7c8bdcf2e1aad6c1cf2998af3
Michelle Laswell <michelle.laswell@gmail.com>
2024-07-21 01:05:20 -0400

    added sqlite database for the menu

--- public/script/navigation.js
+++ public/script/navigation.js
@@ -1,7 +1,7 @@
 // ALL PAGES WITH A NAVIGATION BAR
 
 // SHOWS ACTIVE PAGE IN THE NAVBAR
-
+/*
 document.addEventListener("DOMContentLoaded", () => {
     const currentLocation = location.href;
     const menuItem = document.querySelectorAll('.navbar a');
@@ -11,4 +11,22 @@ document.addEventListener("DOMContentLoaded", () => {
             menuItem[i].className = "active";
         }
     }
+}); */ 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var currentUrl = new URL(window.location.href);
+    var links = document.querySelectorAll('.navbar a');
+    
+    links.forEach(function(link) {
+        var linkUrl = new URL(link.href);
+        
+        if (linkUrl.pathname === currentUrl.pathname) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
 });
+
+
+
